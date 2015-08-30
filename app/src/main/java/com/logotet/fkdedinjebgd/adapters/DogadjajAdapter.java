689eed2cf56ea2651bf0b35f2ef084a183d9a4b9
bobjan <bobjan@sbb.rs>
2015-08-30 21:53:37 +0200

    firts commit

---
+++ app/src/main/java/com/logotet/fkdedinjebgd/adapters/DogadjajAdapter.java
@@ -0,0 +1,41 @@
+package com.logotet.fkdedinjebgd.adapters;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by boban on 8/30/15.
+ */
+public class DogadjajAdapter extends BaseAdapter {
+    Activity activity;
+    ArrayList arrayList;
+
+    public DogadjajAdapter(Activity activity) {
+        this.activity = activity;
+        arrayList = new ArrayList() ;
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
